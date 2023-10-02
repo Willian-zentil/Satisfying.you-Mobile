36015631cd14c86a245aa52db2cb106782062ec4
willianZentil <willian.zentil@maeztra.com>
2023-10-02 15:34:02 -0300

    fix#cads

--- satisfying/src/screens/Home.js
+++ satisfying/src/screens/Home.js
@@ -38,9 +38,9 @@ function Home(props) {
         <SafeAreaView style={viewCards.view}>
           <ScrollView horizontal={true}>
             <Card icon={'devices'} text={'SECOMP 2023'} data={'10/10/2023'} colorIcon={'#704141'} funcao={goToPageEvent} />
-            <Card icon={'groups'} text={'UBUNTU 2022'} data={'05/06/2022'} colorIcon={'#383838'} />
-            <Card icon={'woman'} text={'MENINAS CPU'} data={'01/04/2022'} colorIcon={'#d71616'} />
-            <Card icon={'devices'} text={'TESTE 2023'} data={'10/10/2023'} colorIcon={'#704141'} />
+            <Card icon={'groups'} text={'UBUNTU 2022'} data={'05/06/2022'} colorIcon={'#383838'} funcao={goToPageEvent} />
+            <Card icon={'woman'} text={'MENINAS CPU'} data={'01/04/2022'} colorIcon={'#d71616'} funcao={goToPageEvent} />
+            <Card icon={'devices'} text={'TESTE 2023'} data={'10/10/2023'} colorIcon={'#704141'} funcao={goToPageEvent} />
           </ScrollView>
         </SafeAreaView>
 
